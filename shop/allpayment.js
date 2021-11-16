a7480b377503367d3f5910434aa06ab940ffad71
Pranata <94069522+tatah32@users.noreply.github.com>
2021-11-16 11:31:00 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -2,20 +2,12 @@ const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
 
 *さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
-		
+
+Tidak Tersedia
+
 *さ E-MONEY*
-1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
-2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
-3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+
+Tidak Tersedia
 
 *さ PULSA*
 
